45d5d53e0ff0ad9b15028f8a311e662a39a46684
jeanborech <jeanmiguelborech@gmail.com>
2023-07-07 19:28:30 -0300

    Stack Trace

--- src/TratamentoExceções/Program_TryCatch.java
+++ src/TratamentoExceções/Program_TryCatch.java
@@ -1,5 +1,6 @@
 package TratamentoExceções;
 
+import java.security.KeyStore.TrustedCertificateEntry;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -12,10 +13,11 @@ public class Program_TryCatch {
 		try {
 		String[] vect = sc.nextLine().split(" "); //split usado para dividir uma string em pequenos pedaços
 		int posicao = sc.nextInt();
-		System.out.println(vect[posicao]);
+		System.out.println(vect[posicao]); //vetor na posição digitada
 		}
 		catch (ArrayIndexOutOfBoundsException e) {  //exceção caso tenha posição fora do vetor
             System.out.println("Posição inválida!");
+            e.printStackTrace(); // para mostrar a exceção que está acontecendo 
 		}
 		catch (InputMismatchException e) {  //exceção caso digite um valor da psição que não seja tipo int/número
 			System.out.println("Erro de valor de entrada!");
