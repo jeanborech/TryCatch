ab67f552679b386247f1f386ff1031b6bd1f701a
jeanborech <jeanmiguelborech@gmail.com>
2023-07-07 19:07:38 -0300

    Exceções usando Try Catch

---
+++ src/TratamentoExceções/Program_TryCatch.java
@@ -0,0 +1,29 @@
+package TratamentoExceções;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Program_TryCatch {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+        
+		try {
+		String[] vect = sc.nextLine().split(" "); //split usado para dividir uma string em pequenos pedaços
+		int posicao = sc.nextInt();
+		System.out.println(vect[posicao]);
+		}
+		catch (ArrayIndexOutOfBoundsException e) {  //exceção caso tenha posição fora do vetor
+            System.out.println("Posição inválida!");
+		}
+		catch (InputMismatchException e) {  //exceção caso digite um valor da psição que não seja tipo int/número
+			System.out.println("Erro de valor de entrada!");
+		}
+		
+		System.out.println("Fim do programa");
+		
+		sc.close();
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Jean Miguel Borech
+ *
+ */
+module TryCatch {
+}
